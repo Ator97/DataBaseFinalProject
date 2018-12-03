95fc080d21fb9988656fcf7ae1f84e4b909174ca
Ator97 <oscargutcas9705@gmail.com>
2018-12-02 22:25:27 -0600

    Se cambia orden de carga

--- s-00-main.sql
+++ s-00-main.sql
@@ -38,10 +38,9 @@ end;
 connect as VAGU_proy_admin;
 
 
-
+@./s-04-tablas-externas.sql
 @./s-02-entidades.sql
 @./s-03-tablas-temporales.sql
-@./s-04-tablas-externas.sql
 @./s-05-secuencias.sql
 @./s-06-índices.sql
 @./s-07-sinonimos.sql
