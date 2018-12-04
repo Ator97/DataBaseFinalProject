c52b3d9c993481dce6712b961254afb5459ab1fa
Ator97 <oscargutcas9705@gmail.com>
2018-12-03 19:36:46 -0600

    Nuevo caso de tabla externa

--- s-04-tablas-externas.sql
+++ s-04-tablas-externas.sql
@@ -17,3 +17,36 @@ create or replace directory tmp_dir as '/tmp/proy_bases';
 grant read, write on directory tmp_dir to VAGU_proy_admin;
 grant read on directory tmp_dir to VAGU_proy_invitado;
 
+
+--
+--ANTIGÜAMENTE SE POSEÍA UN SISTEMA QUE MANTENIA UN ARCHIVO
+--DE TEXTO QUE CONTENÍA EL SALARIO DE TODOS LOS PROFESORES DE
+--ASIGNATURA, LA ADMINISTRACION, HA OPTADO DE MOMENTO MANTENER ESTE
+--ARCHIVO DE FORMA EXTERNA
+--
+
+
+
+CREATE TABLE SALARIO_PROFESOR_ASIGNATURA( --CATALOGO DE SALARIOS
+  PROFESOR_ASIGNATURA_ID  NUMBER(6,0)  NOT NULL,
+  SALARIO_MENSUAL         NUMBER(7,2)  NOT NULL,
+)
+organization external (
+--En oracle existen 2 tipos de drivers para parsear el archivo: -- oracle_loader y oracle_datapump
+type oracle_loader
+default directory tmp_dir
+    access parameters (
+        records delimited by newline
+        badfile tmp_dir:'salario_profesor_asignatura.log'
+        logfile tmp_dir:'salario_profesor_asignatura.log'
+        fields terminated by ','
+        lrtrim
+        missing field values are null
+        (
+        DIA_SEMANA_ID,DESCRIPCION
+        )
+)
+    location ('salario_profesor_asignatura.csv')
+)
+reject limit unlimited;
+;
