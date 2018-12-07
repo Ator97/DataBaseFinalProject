536e386d6cb2bab87b100658710f6cf5809eb451
Oscar Gutiérrez Castillo <oscar.gutierrezcastillo@comunidad.unam.mx>
2018-12-06 18:01:30 -0600

    Update s-14-p-1-prueba.sql

--- s-14-p-1-prueba.sql
+++ s-14-p-1-prueba.sql
@@ -1,3 +1,7 @@
 --@Autor(es):       Gutiérrez Castillo Oscar, Valderrama Navarro Armando
 --@Fecha creación:  01/12/2018
---@Descripción:     <breve descripción del contenido y propósito del archivo>
+--@Descripción:     validador del s-13
+
+
+--Validacion
+exec p_imparticiones('MAPE704532G45');
